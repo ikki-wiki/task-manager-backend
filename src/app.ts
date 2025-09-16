e3ffd43fbdaacc6bd14e7ac526b62259316e4a09
ikki-wiki <2013620@student.uma.pt>
2025-09-16 19:29:27 +0100

    fix 3 tests

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 5000;
 // ✅ Middlewares
 const allowedOrigins = [
   "http://localhost:5173", // local dev
-  "https://task-manager-frontend-psi-neon.vercel.app/" // your live site
+  "https://task-manager-frontend-psi-neon.vercel.app" // your live site
 ];
 
 app.use(cors({
