798da1c04dcb8214bfa3d6b5b2f9295a90ba3d3b
ikki-wiki <2013620@student.uma.pt>
2025-09-18 15:15:25 +0100

    Return to localhost

--- src/app.ts
+++ src/app.ts
@@ -7,15 +7,8 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-const allowedOrigins = [
-  "http://localhost:5173", // local dev
-  process.env.FRONTEND_URL?.trim() // production frontend
-];
-
 app.use(cors({
-  origin: process.env.NODE_ENV === "production"
-    ? process.env.FRONTEND_URL
-    : "http://localhost:5173",
+  origin: process.env.FRONTEND_URL,
   credentials: true
 }));
 
